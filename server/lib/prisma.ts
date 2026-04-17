3d7d9dccdb71b232645031c2ca864af7640879cf
Noyaindustries <services@noyaindustries.com>
2026-04-17 15:00:14 +0000

    caisse

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+if (!process.env.DATABASE_URL || process.env.DATABASE_URL.trim() === '') {
+  process.env.DATABASE_URL = 'mongodb://127.0.0.1:27017/caisseci'
+}
+
 export const prisma = new PrismaClient()
